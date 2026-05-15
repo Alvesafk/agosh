f351d2190c875e43ddca1ae387c67d94e4ef2915
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-14 22:32:45 -0300

    cd now has the ~ alias of home

--- src/main.go
+++ src/main.go
@@ -9,21 +9,43 @@ import (
 	"strings"
 )
 
+func getUserHomeDir() string {
+	home_dir, err := os.UserHomeDir()
+	if err != nil {
+		return ""
+	}
+
+	return home_dir
+}
+
+func getHostName() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return ""
+	}
+
+	return hostname
+}
+
 func handleInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 
 	args := strings.Split(input, " ")
 
 	switch args[0] {
-		case "cd":
-			if len(args) < 2 {
-				return errors.New("Path is required!")
-			}
+	case "cd":
+		if args[1] == "~" {
+			return os.Chdir(getUserHomeDir())
+		}
 
-			return os.Chdir(args[1])
-		
-		case "exit":
-			os.Exit(0)
+		if len(args) < 2 {
+			return errors.New("Path is required!")
+		}
+
+		return os.Chdir(args[1])
+
+	case "exit":
+		os.Exit(0)
 	}
 
 	cmd := exec.Command(args[0], args[1:]...)
@@ -35,10 +57,13 @@ func handleInput(input string) error {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
-
 	reader := bufio.NewReader(os.Stdin)
+	
+	user := "afk"
+
 	for {
+		fmt.Printf("%s on ionknow\n$ ", user)
+
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
