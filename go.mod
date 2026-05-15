025d78577ec977bdc315df8981d0e85fd0cf42c7
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-14 22:05:38 -0300

    simple shell implementation of a shell on go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Alvesafk
+
+go 1.26.2
---
+++ src/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func handleInput(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+
+	cmd := exec.Command(input)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err = handleInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
