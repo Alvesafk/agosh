f15674ea8138f6f9ec51a2d79332b1aaaa2d733f
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-15 09:59:30 -0300

    added the home aliases of ~

--- src/main.go
+++ src/main.go
@@ -13,6 +13,7 @@ import (
 var (
 	last_working_directory = getUserHomeDir()
 	history_absolute_path = getUserHomeDir() + "/.gosh_history"
+	command_index = 0
 ) 
 
 func main() {
@@ -84,6 +85,12 @@ func handleInput(input string, history_file *os.File) error {
 	}
 
 	args := strings.Split(input, " ")
+
+	if len(args) >= 2 {
+		if strings.Contains(args[1], "~") {
+			args[1] = strings.Replace(args[1], "~", getUserHomeDir(), 1)
+		}
+	}
 
 	switch args[0] {
 	case "cd":
